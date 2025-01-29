3bf7604d23902026c42e4545cf07f339d34606de
장은우 <oxygenic314@gmail.com>
2025-01-29 20:17:42 +0900

    DHT_multi.cpp 업데이트

--- dht_master/DHT_multi.cpp
+++ dht_master/DHT_multi.cpp
@@ -51,10 +51,6 @@ bool DHTMulti::requestSensorData(uint8_t slaveId, char* buffer) {
       HC12.readBytes(buffer, 24);
       return validateReceivedData(buffer, slaveId);
     }
-		// read로 쌓인 버퍼 초기화
-		while (HC12.available()) {
-			HC12.read();
-		}
   }
 
   return false; // Timed out
@@ -72,10 +68,6 @@ bool DHTMulti::requestResendData(uint8_t slaveId, char* buffer) {
       HC12.readBytes(buffer, 24);
       return validateReceivedData(buffer, slaveId);
     }
-		// read로 쌓인 버퍼 초기화
-		while (HC12.available()) {
-			HC12.read();
-		}
   }
 
   return false; // Timed out
