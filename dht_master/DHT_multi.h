823cb82313b912904abbc2394201c7bbc116755b
장은우 <oxygenic314@gmail.com>
2025-02-05 16:01:45 +0900

    Add files via upload

---
+++ dht_master/DHT_multi.h
@@ -0,0 +1,26 @@
+#ifndef DHT_MULTI_H
+#define DHT_MULTI_H
+
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+extern const int temp_slave_Amount;  // slave 아두이노 개수 
+
+class DHTMulti {
+public:
+  
+  DHTMulti(uint8_t slaveAmount);
+  
+  char* getAllSensorData();
+
+private:
+  uint8_t slaveAmount;
+  char* combinedData;
+
+  bool requestSensorData(uint8_t slaveId, char* buffer);
+  bool validateReceivedData(const char* data, uint8_t slaveId);
+};
+
+extern SoftwareSerial hc12; // 메인에서 선언된 hc12 사용
+
+#endif
